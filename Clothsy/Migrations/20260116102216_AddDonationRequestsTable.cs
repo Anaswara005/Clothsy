856774136e726c73a82729fac4666c22c6f46bb4
kpana <kpanaswara05@gmail.com>
2026-01-16 15:53:08 +0530

    Add DonationRequests table migration

---
+++ Clothsy/Migrations/20260116102216_AddDonationRequestsTable.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clothsy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDonationRequestsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1023));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1027));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1029));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1031));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1032));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1034));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1035));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1037));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1038));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1040));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1041));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1042));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1044));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 16, 10, 22, 16, 218, DateTimeKind.Utc).AddTicks(1045));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(951));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(956));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(958));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(961));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(962));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(964));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(966));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(968));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(969));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(971));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(973));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(974));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(976));
+
+            migrationBuilder.UpdateData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "CreatedAt",
+                value: new DateTime(2026, 1, 13, 19, 55, 58, 180, DateTimeKind.Utc).AddTicks(977));
+        }
+    }
+}
